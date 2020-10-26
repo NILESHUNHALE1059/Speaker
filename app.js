bbc92b90ee99ce57132348d433aacd5773fd126d
Nilesh Unhale <nileshunhale1059@gmail.com>
2020-10-26 22:20:48 +0530

    My first git commit
    Feels awsome

---
+++ app.js
@@ -0,0 +1,47 @@
+let a = document.querySelector(".a");
+// a.classList.add("block");
+
+// a.append("adadsd");
+
+// var lineBreak = a.append('<br/>');
+// document.documentElement.scroll
+let ip= document.getElementById("ip");
+let js= document.getElementById("js");
+
+ip.placeholder="fef";
+
+var person = {
+    
+    "Speakers":
+    [{
+        name: "John",
+        lastName : "Doe",
+        id       : 5566
+    },
+    {
+        name: "John",
+        lastName : "Doe",
+        id       : 5566
+    },
+    {
+        name: "John",
+        lastName : "Doe",
+        id       : 5566
+    },
+    {
+    name: "John",
+    lastName : "Doe",
+    id       : 5566 
+}]
+
+
+  };
+
+//   let sq=person.Speakers.map(myFunc);
+  for(let item in person.Speakers){
+      
+      a.appendChild('<li>' + person.Speakers[item].name+'</li>');
+
+   
+
+ }
